e9dd6af59599af5da864f5dc140d2a358bdbdf99
bramba2000 <bramba2000@gmail.com>
2023-08-09 23:30:27 +0200

    Add Main class as entry point of the jar

---
+++ src/main/java/it/polimi/ds/Main.java
@@ -0,0 +1,9 @@
+package it.polimi.ds;
+
+import it.polimi.ds.vsync.VSynchLayer;
+
+public class Main {
+    public static void main(String[] args) {
+        VSynchLayer vSynchLayer = new VSynchLayer();
+    }
+}
