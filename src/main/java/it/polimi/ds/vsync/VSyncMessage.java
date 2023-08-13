d51858cd7feeeecc442a143cc1acc2085aac164d
Paolo Cerutti <paolo2.cerutti@mail.polimi.it>
2023-08-13 14:01:20 +0200

    added a payload in VSyncMessage.java

--- src/main/java/it/polimi/ds/vsync/VSyncMessage.java
+++ src/main/java/it/polimi/ds/vsync/VSyncMessage.java
@@ -1,6 +1,11 @@
 package it.polimi.ds.vsync;
-
 import it.polimi.ds.reliability.KnowledgeableMessage;
 
 public class VSyncMessage extends KnowledgeableMessage {
+
+    private byte[] payload;
+
+    public VSyncMessage(byte[] payload) {
+        this.payload = payload;
+    }
 }
