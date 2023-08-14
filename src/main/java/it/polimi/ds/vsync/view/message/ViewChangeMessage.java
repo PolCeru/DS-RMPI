a69d82ef1d93c7b2f580fdf272fa8fd7d83d9eb1
bramba2000 <bramba2000@gmail.com>
2023-08-14 16:29:57 +0200

    Fix missing enum const for ViewChangeMessage

--- src/main/java/it/polimi/ds/vsync/view/message/ViewChangeMessage.java
+++ src/main/java/it/polimi/ds/vsync/view/message/ViewChangeMessage.java
@@ -1,4 +1,7 @@
 package it.polimi.ds.vsync.view.message;
 
 public class ViewChangeMessage extends ViewManagerMessage {
+    public ViewChangeMessage() {
+        super(ViewChangeType.VIEW_CHANGE);
+    }
 }
--- src/main/java/it/polimi/ds/vsync/view/message/ViewChangeType.java
+++ src/main/java/it/polimi/ds/vsync/view/message/ViewChangeType.java
@@ -3,6 +3,8 @@ package it.polimi.ds.vsync.view.message;
 public enum ViewChangeType {
     INIT_VIEW("InitialTopologyMessage"),
     CONFIRM("ConfirmViewChangeMessage"),
+
+    VIEW_CHANGE("ViewChangeMessage"),
     ADVERTISE("AdvertiseMessage");
     public final String className;
 
