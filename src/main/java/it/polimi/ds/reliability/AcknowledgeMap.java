0869372c1f2eac139bd2a7374ac63c326fbb2765
bramba2000 <bramba2000@gmail.com>
2023-08-18 23:09:06 +0200

    Remake of AcknowledgeMap for all host ack support

--- src/main/java/it/polimi/ds/reliability/AcknowledgeMap.java
+++ src/main/java/it/polimi/ds/reliability/AcknowledgeMap.java
@@ -4,29 +4,71 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AcknowledgeMap {
-    final Map<UUID, Map<UUID, Boolean>> ackMap = new HashMap<>();
+    final Map<UUID, MessageState> ackMap = new HashMap<>();
 
-    synchronized public void newBroadcastMessage(UUID messageUUID, List<UUID> recipients) {
-        HashMap<UUID, Boolean> map = new HashMap<>();
-        recipients.forEach((id) -> map.put(id, Boolean.FALSE));
-        ackMap.put(messageUUID, map);
+    public void sendMessage(UUID messageId, List<UUID> recipients) {
+        ackMap.put(messageId, MessageState.ofMessage(recipients));
     }
 
-    synchronized public void ackMessage(UUID messageId, UUID receiver) {
-        ackMap.get(messageId).put(receiver, Boolean.TRUE);
+    public void receiveMessage(UUID messageId, List<UUID> recipients) {
+        if (isPresent(messageId)) {
+            ackMap.get(messageId).setMessageReceived();
+        } else {
+            ackMap.put(messageId, MessageState.ofMessage(recipients));
+        }
     }
 
-    synchronized public boolean isAckByEveryone(UUID messageId) {
-        return ackMap.get(messageId).values().stream().anyMatch((val) -> val.equals(Boolean.FALSE));
+    private boolean isPresent(UUID messageId) {
+        return ackMap.containsKey(messageId);
     }
 
-    synchronized public void removeMessage(UUID messageId) {
-        ackMap.remove(messageId);
+    public void receiveAck(UUID messageId, UUID ackHostId, List<UUID> recipients) {
+        if (isPresent(messageId)) {
+            ackMap.get(messageId).setAck(ackHostId);
+        } else {
+            ackMap.put(messageId, MessageState.ofAck(ackHostId, recipients));
+        }
     }
 
-    synchronized public List<UUID> missingAck(UUID messageId) {
-        return ackMap.get(messageId).entrySet().stream().filter(entry -> entry.getValue().equals(Boolean.FALSE)).map(Map.Entry::getKey).toList();
+    public boolean isComplete(UUID messageId) {
+        MessageState state = ackMap.get(messageId);
+        return state.messageReceived && state.ackMap.values().stream().allMatch(Boolean.TRUE::equals);
+    }
+
+    public List<UUID> missingAcks(UUID messageId) {
+        MessageState state = ackMap.get(messageId);
+        return state.ackMap.entrySet().stream().filter(entry -> entry.getValue().equals(Boolean.FALSE)).map(Map.Entry::getKey).toList();
+    }
+
+    private static class MessageState {
+        private boolean messageReceived;
+        private final Map<UUID, Boolean> ackMap;
+
+        private MessageState(boolean messageReceived, Map<UUID, Boolean> ackMap) {
+            this.messageReceived = messageReceived;
+            this.ackMap = ackMap;
+        }
+
+        public static MessageState ofMessage(List<UUID> recipients) {
+            return new MessageState(true, recipients.stream().collect(Collectors.toMap(Function.identity(), ignored -> Boolean.FALSE)));
+        }
+
+        public static MessageState ofAck(UUID senderUid, List<UUID> recipients) {
+            Map<UUID, Boolean> hashMap = recipients.stream().collect(Collectors.toMap(Function.identity(), ignored -> Boolean.FALSE));
+            hashMap.put(senderUid, Boolean.TRUE);
+            return new MessageState(false, hashMap);
+        }
+
+        public void setAck(UUID id) {
+            this.ackMap.put(id, Boolean.TRUE);
+        }
+
+        public void setMessageReceived() {
+            this.messageReceived = true;
+        }
     }
 }
